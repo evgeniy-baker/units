75e439e4e6f462cfeaf79fc8a99a0433c5983187
evgeniy-baker <evgenie_1994@mail.ru>
2023-02-17 16:19:33 +0300

    14 задача +

--- unit_03/unit_03.js
+++ unit_03/unit_03.js
@@ -205,10 +205,28 @@ document.querySelector('.b-13').onclick = f13;
 
 
 // Task 14
-// Дан input i-141 и input-142, type=number.  Дан select s-143, который содержит две операции - +, -, *, / . Дана кнопка b-14, при нажатии на которую срабатывает функция f14. Функция выводит в out-14 результат операций выбранной в 3-м select к числам введенным в первом и втором input. Например выбрано 1 13 +, нужно вывести результат операции 1+13 т.е.  14.
+// Дан input i-141 и input-142, type=number.  Дан select s-143, который содержит две операции - +, -, *, / . 
+// Дана кнопка b-14, при нажатии на которую срабатывает функция f14. Функция выводит в out-14 результат 
+// операций выбранной в 3-м select к числам введенным в первом и втором input. Например выбрано 1 13 +, 
+// нужно вывести результат операции 1+13 т.е.  14.
 
 function f14(){
-
+    let num1 = +document.querySelector('.i-141').value
+    let num2 = +document.querySelector('.i-142').value
+    let select = document.querySelector('.s-143').value
+    let out = document.querySelector('.out-14')
+    if (select == '+'){
+        out.innerHTML = num1 + num2
+    }
+    else if (select == '-'){
+        out.innerHTML = num1 - num2
+    }
+    else if (select == '*'){
+        out.innerHTML = num1 * num2
+    }
+    else if (select == '/'){
+        out.innerHTML = num1 / num2
+    }
 }
 
 document.querySelector('.b-14').onclick = f14;
