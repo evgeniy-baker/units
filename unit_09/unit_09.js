8da4f5440ca2fadb16ccc481a5c808c357753de0
evgeniy-baker <evgenie_1994@mail.ru>
2023-03-02 20:38:46 +0300

    DOM все задачи решены +

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -266,19 +266,28 @@ function f18() {
 document.querySelector('.b-18').onclick = f18;
 
 //  Task 19
-// Добавьте кнопку .b-19, которая запускает функцию f19. Функция с помощью getAttribute получает data-b атрибут с параграфов p-19 и выводит в out-19 через пробел. Обратите внимание, что элементов p-19 больше одного.
+// Добавьте кнопку .b-19, которая запускает функцию f19. 
+// Функция с помощью getAttribute получает data-b атрибут с параграфов p-19 и выводит в out-19 через пробел. 
+// Обратите внимание, что элементов p-19 больше одного.
 
 function f19() {
-
+    let out = document.querySelector('.out-19');
+    let p = document.querySelectorAll('.p-19');
+    for (let i = 0; i < p.length; i++) {
+        out.innerHTML += p[i].getAttribute('data-b') + ' '
+    }
 }
 
 document.querySelector('.b-19').onclick = f19;
 
 //  Task 20
-// Добавьте кнопку .b-20, которая запускает функцию f20. Функция с помощью setAttribute присваивает атрибут title="go" в div.out-20.
+// Добавьте кнопку .b-20, которая запускает функцию f20. 
+// Функция с помощью setAttribute присваивает атрибут title="go" в div.out-20.
 
 function f20() {
-
+    let out = document.querySelector('.out-20');
+    out.setAttribute('title', 'go');
+    out.innerHTML = out.getAttribute('title');
 }
 
 document.querySelector('.b-20').onclick = f20;
