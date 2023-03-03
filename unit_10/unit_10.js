55faa26c7283e4802d62a15dabef933638db728b
evgeniy-baker <evgenie_1994@mail.ru>
2023-03-03 17:42:34 +0300

    Массивы 9 +

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -155,6 +155,7 @@ document.querySelector('.b-8').onclick = f8;
 let ar9 = [100, 200, 300, 400, 700, 121];
 
 function f9() {
+    document.querySelector('.out-9').innerHTML = ar9[ar9.length - 1];
 }
 
 document.querySelector('.b-9').onclick = f9;
